9d058411a9200caae39be6e83df5a3074cf46a43
Joey Quadros <joey_qpb@hotmail.com>
2023-03-01 20:41:42 -0300

    feat: add user login route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import { UserModel } from "../model/user.model.js";
+import { generateToken } from "../config/jwt.config.js";
 
 const userRouter = express.Router();
 
@@ -23,6 +24,34 @@ userRouter.post("/signup", async (req, res) => {
 
     delete newUser._doc.passwordHash;
     return res.status(200).json(newUser);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+userRouter.post("/login", async (req, res) => {
+  try {
+    const { name, password } = req.body;
+
+    if (!name || !password)
+      return res.status(400).json({ msg: "fill in all fields" });
+
+    const user = await UserModel.findOne({ name });
+
+    if (!(await bcrypt.compare(password, user.passwordHash)))
+      return res.status(404).json({ msg: "invalid email or password " });
+
+    const token = generateToken(user);
+
+    return res.status(200).json({
+      user: {
+        id: user._id,
+        name: user.name,
+        role: user.role,
+      },
+      token: token,
+    });
   } catch (err) {
     console.log(err);
     return res.status(500).json(err);
