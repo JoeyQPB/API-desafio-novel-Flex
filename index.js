3906a5f10ef42d5b6dd4f5537c473770909e449f
Joey Quadros <joey_qpb@hotmail.com>
2023-03-01 18:09:00 -0300

    feat: using mongoose to dbconnect

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import * as dotenv from "dotenv";
+import { dbConnect } from "./config/db.config.js";
 
 dotenv.config();
-
 const app = express();
+dbConnect();
 
 app.listen(Number(process.env.PORT), () => {
   console.log(`server running at port ${process.env.PORT}`);
