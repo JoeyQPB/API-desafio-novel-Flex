0e7fa1a235ac3e017efac56c16c4dfcea56f3db8
Joey Quadros <joey_qpb@hotmail.com>
2023-03-01 20:32:53 -0300

    feat: add unique especify to name column

--- model/user.model.js
+++ model/user.model.js
@@ -1,7 +1,7 @@
 import { model, Schema, Types } from "mongoose";
 
 const userSchema = new Schema({
-  name: { type: String, required: true, trim: true },
+  name: { type: String, required: true, unique: true, trim: true },
   passwordHash: { type: String, required: true },
   role: { type: String, enum: ["ADMIN", "USER"], default: "USER" },
   products: [{ type: Types.ObjectId, ref: "Product" }],
