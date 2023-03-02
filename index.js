182e48a57e4a33f63753e4c897b3d33fe473c05a
Joey Quadros <joey_qpb@hotmail.com>
2023-03-02 06:08:24 -0300

    feat: add uploadImgRouter

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import * as dotenv from "dotenv";
 import { dbConnect } from "./config/db.config.js";
 import { userRouter } from "./routes/user.routes.js";
 import { productRouter } from "./routes/product.routes.js";
+import { uploadImgRouter } from "./routes/upload-image.routes.js";
 
 dotenv.config();
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.json());
 
 app.use(`/user`, userRouter);
 app.use(`/product`, productRouter);
+app.use(`/upload_image`, uploadImgRouter);
 
 app.listen(Number(process.env.PORT), () => {
   console.log(`server running at port ${process.env.PORT}`);
