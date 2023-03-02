9848e715507473e2b402249cf7dad2c82f8b00e3
Joey Quadros <joey_qpb@hotmail.com>
2023-03-02 16:53:18 -0300

    feat: remove limiter

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,7 +5,6 @@ import { signupUserController } from "../controllers/users/signup.controller.js"
 import { loginUserController } from "../controllers/users/login.controller.js";
 import { listUserController } from "../controllers/users/list.controller.js";
 import { profileUserController } from "../controllers/users/profile.controller.js";
-import { apiLimiter } from "../middlewares/limiter.js";
 
 const userRouter = express.Router();
 
@@ -13,7 +12,7 @@ userRouter.post("/signup", signupUserController.signup);
 
 userRouter.post("/login", loginUserController.login);
 
-userRouter.get("/list", apiLimiter, listUserController.list);
+userRouter.get("/list", listUserController.list);
 
 userRouter.get(
   "/profile",
