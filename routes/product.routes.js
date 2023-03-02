c6b62f3cbad79ab6276b7c92c3003d7a9506f623
Joey Quadros <joey_qpb@hotmail.com>
2023-03-02 01:36:14 -0300

    feat: add list product route

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -54,4 +54,14 @@ productRouter.post(
   }
 );
 
+productRouter.get("/list", isAuth, attachCurrentUser, async (req, res) => {
+  try {
+    const products = await ProductModel.find({});
+    return res.status(200).json(products);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
 export { productRouter };
