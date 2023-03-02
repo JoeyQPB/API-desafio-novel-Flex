e2b2420af61d33572b06f39b9b0634a5f65a0338
Joey Quadros <joey_qpb@hotmail.com>
2023-03-02 05:17:58 -0300

    feat: add login users controller

---
+++ controllers/users/login.controller.js
@@ -0,0 +1,33 @@
+import bcrypt from "bcrypt";
+import { UserModel } from "../../model/user.model.js";
+import { generateToken } from "../../config/jwt.config.js";
+
+export const loginUserController = {
+  async login(req, res) {
+    try {
+      const { name, password } = req.body;
+
+      if (!name || !password)
+        return res.status(400).json({ msg: "fill in all fields" });
+
+      const user = await UserModel.findOne({ name });
+
+      if (!(await bcrypt.compare(password, user.passwordHash)))
+        return res.status(404).json({ msg: "invalid email or password " });
+
+      const token = generateToken(user);
+
+      return res.status(200).json({
+        user: {
+          id: user._id,
+          name: user.name,
+          role: user.role,
+        },
+        token: token,
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+};
