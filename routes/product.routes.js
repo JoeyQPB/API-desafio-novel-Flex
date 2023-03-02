77c0e89a1053197e287aa74b167cb9ccac617ff6
Joey Quadros <joey_qpb@hotmail.com>
2023-03-02 01:55:11 -0300

    feat: add update partial product route

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -97,4 +97,27 @@ productRouter.put(
   }
 );
 
+productRouter.patch(
+  "/update_partial/:id",
+  isAuth,
+  attachCurrentUser,
+  async (req, res) => {
+    try {
+      const loggedInUser = req.currentUser;
+      const updatedPartialProduct = await ProductModel.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          ...req.body,
+          $push: { updatedAt: Date.now(), updatedBy: loggedInUser._id },
+        },
+        { new: true, runValidators: true }
+      );
+      return res.status(200).json(updatedPartialProduct);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  }
+);
+
 export { productRouter };
