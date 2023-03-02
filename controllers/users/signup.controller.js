a8d3be2f9fe1dffdd85dfae1b9d9f5fd447474d8
Joey Quadros <joey_qpb@hotmail.com>
2023-03-02 05:18:18 -0300

    feat: add signup users controller

---
+++ controllers/users/signup.controller.js
@@ -0,0 +1,26 @@
+import bcrypt from "bcrypt";
+import { UserModel } from "../../model/user.model.js";
+
+export const signupUserController = {
+  async signup(req, res) {
+    try {
+      const { password } = req.body;
+      if (!password)
+        return res.status(400).json({ msg: "Password is required" });
+
+      const salt = await bcrypt.genSalt(Number(process.env.SALT_ROUNDS));
+      const hashedPassword = await bcrypt.hash(password, salt);
+
+      const newUser = await UserModel.create({
+        ...req.body,
+        passwordHash: hashedPassword,
+      });
+
+      delete newUser._doc.passwordHash;
+      return res.status(200).json(newUser);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+};
