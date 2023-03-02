c32c177c3a785962f0edcfd2fbc03423d33a691e
Joey Quadros <joey_qpb@hotmail.com>
2023-03-01 23:16:15 -0300

    feat: import and use productRouter

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import * as dotenv from "dotenv";
 import { dbConnect } from "./config/db.config.js";
 import { userRouter } from "./routes/user.routes.js";
+import { productRouter } from "./routes/product.routes.js";
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,7 @@ dbConnect();
 app.use(express.json());
 
 app.use(`/user`, userRouter);
+app.use(`/product`, productRouter);
 
 app.listen(Number(process.env.PORT), () => {
   console.log(`server running at port ${process.env.PORT}`);
