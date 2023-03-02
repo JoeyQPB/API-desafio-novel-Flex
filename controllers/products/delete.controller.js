e6431a4756bc37a88b33d4468cc592ff66a6f274
Joey Quadros <joey_qpb@hotmail.com>
2023-03-02 05:16:30 -0300

    feat: add delete product controller

---
+++ controllers/products/delete.controller.js
@@ -0,0 +1,15 @@
+import { ProductModel } from "../../model/product.model.js";
+
+export const deleteProductController = {
+  async delete(req, res) {
+    try {
+      const deleteProduct = await ProductModel.deleteOne({
+        _id: req.params.id,
+      });
+      return res.status(200).json(deleteProduct);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+};
