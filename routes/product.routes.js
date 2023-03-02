bf9f952993ddd809f955b43926cb0f52458e4fed
Joey Quadros <joey_qpb@hotmail.com>
2023-03-02 16:17:03 -0300

    feat: implements list by name controller

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { createProductController } from "../controllers/products/create.controller.js";
 import { deleteProductController } from "../controllers/products/delete.controller.js";
+import { listProductByNameController } from "../controllers/products/list-by-name.controller.js";
 import { listProductByPriceController } from "../controllers/products/list-by-price.controller.js";
 import { listProductController } from "../controllers/products/list.controller.js";
 import { showProductController } from "../controllers/products/show.controller.js";
@@ -9,7 +10,6 @@ import { updateProductController } from "../controllers/products/update.controll
 import attachCurrentUser from "../middlewares/attachCurrentUser.js";
 import { isAdmin } from "../middlewares/isAdmin.js";
 import isAuth from "../middlewares/isAuth.js";
-import { ProductModel } from "../model/product.model.js";
 
 const productRouter = express.Router();
 
@@ -29,19 +29,12 @@ productRouter.get(
 );
 
 // order by name
-productRouter.get("/list_name", isAuth, attachCurrentUser, async (req, res) => {
-  try {
-    const products = await ProductModel.find({});
-    if (!products)
-      return res.status(404).json({ msg: "no product has been registered" });
-
-    const listOrderByName = products.sort(products.name);
-    return res.status(200).json(listOrderByName);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json(err);
-  }
-});
+productRouter.get(
+  "/list_name",
+  isAuth,
+  attachCurrentUser,
+  listProductByNameController.handle
+);
 
 productRouter.get(
   "/list_price",
