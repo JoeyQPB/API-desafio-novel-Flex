62a9b09ac20dd2ee2e12991f5f60b50ea9034c4e
Joey Quadros <joey_qpb@hotmail.com>
2023-03-02 15:54:10 -0300

    feat: add list by price controller

---
+++ controllers/products/list-by-price.controller.js
@@ -0,0 +1,17 @@
+import { ProductModel } from "../../model/product.model.js";
+
+export const listProductByPriceController = {
+  async handle(req, res) {
+    try {
+      const products = await ProductModel.find({});
+      if (!products)
+        return res.status(404).json({ msg: "no product has been registered" });
+
+      const listOrderByName = products.sort((a, b) => a.price - b.price);
+      return res.status(200).json(listOrderByName);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+};
