37b245d9e6f273d681e5e4a3aa44cbce039f5c2a
Joey Quadros <joey_qpb@hotmail.com>
2023-03-02 05:17:17 -0300

    feat: add update product controller

---
+++ controllers/products/update.controller.js
@@ -0,0 +1,32 @@
+import { ProductModel } from "../../model/product.model.js";
+import { validateFields } from "../../utils/requeridFields.js";
+
+export const updateProductController = {
+  async update(req, res) {
+    const { error, msg } = validateFields(
+      req,
+      ["name", "description"],
+      ["price"]
+    );
+
+    if (error) {
+      return res.status(400).json({ msg });
+    }
+
+    try {
+      const loggedInUser = req.currentUser;
+      const updatedProduct = await ProductModel.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          ...req.body,
+          $push: { updatedAt: Date.now(), updatedBy: loggedInUser._id },
+        },
+        { new: true, runValidators: true }
+      );
+      return res.status(200).json(updatedProduct);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+};
