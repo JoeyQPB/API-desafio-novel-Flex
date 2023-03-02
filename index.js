61d2eadefa5e550ac9c9c548719080e8fc8ab1e0
Joey Quadros <joey_qpb@hotmail.com>
2023-03-02 16:53:02 -0300

    feat: implements limiter

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ import { dbConnect } from "./config/db.config.js";
 import { userRouter } from "./routes/user.routes.js";
 import { productRouter } from "./routes/product.routes.js";
 import { uploadImgRouter } from "./routes/upload-image.routes.js";
+import { apiLimiter } from "./middlewares/limiter.js";
 
 dotenv.config();
 const app = express();
 dbConnect();
 app.use(express.json());
 
-app.use(`/user`, userRouter);
-app.use(`/product`, productRouter);
-app.use(`/upload_image`, uploadImgRouter);
+app.use(`/user`, apiLimiter, userRouter);
+app.use(`/product`, apiLimiter, productRouter);
+app.use(`/upload_image`, apiLimiter, uploadImgRouter);
 
 app.listen(Number(process.env.PORT), () => {
   console.log(`server running at port ${process.env.PORT}`);
