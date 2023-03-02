4b7ff959be3cf76e00445cee8e5640482622e2d7
Joey Quadros <joey_qpb@hotmail.com>
2023-03-02 05:16:40 -0300

    feat: add list product controller

---
+++ controllers/products/list.controller.js
@@ -0,0 +1,15 @@
+import { ProductModel } from "../../model/product.model.js";
+
+export const listProductController = {
+  async list(req, res) {
+    try {
+      const products = await ProductModel.find({});
+      if (!products)
+        return res.status(404).json({ msg: "no product has been registered" });
+      return res.status(200).json(products);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+};
