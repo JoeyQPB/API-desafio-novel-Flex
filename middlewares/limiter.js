e317cf5de8c3d50517a57f641d350ab17491dd66
Joey Quadros <joey_qpb@hotmail.com>
2023-03-02 16:52:47 -0300

    feat: change max of requests

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const apiLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minutes
-  max: 20, // Limit each IP to 2 requests per `window` (here, per 1 minutes)
+  max: 40, // Limit each IP to 2 requests per `window` (here, per 1 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
