0f112ec7c49f4bfe39fe9dd2642d4f8a682b2677
Joey Quadros <joey_qpb@hotmail.com>
2023-03-02 01:28:27 -0300

    feat: add create product route

---
+++ routes/product.routes.js
@@ -0,0 +1,57 @@
+import express from "express";
+import attachCurrentUser from "../middlewares/attachCurrentUser.js";
+import { isAdmin } from "../middlewares/isAdmin.js";
+import isAuth from "../middlewares/isAuth.js";
+import { ProductModel } from "../model/product.model.js";
+import { UserModel } from "../model/user.model.js";
+
+const productRouter = express.Router();
+
+productRouter.get("/", (req, res) => {
+  return res.status(200).json({ msg: "ok" });
+});
+
+productRouter.post(
+  "/create_product",
+  isAuth,
+  attachCurrentUser,
+  isAdmin,
+  async (req, res) => {
+    try {
+      const requireFields = ["name", "description"];
+      let error = false;
+      let fieldError;
+      requireFields.forEach((field) => {
+        if (!req.body[`${field}`] || typeof req.body[`${field}`] !== "string") {
+          error = true;
+          fieldError = field;
+        }
+      });
+
+      if (error) {
+        return res.status(400).json({ msg: `Can not defined ${fieldError}` });
+      }
+
+      if (typeof req.body.price !== "number")
+        return res.status(400).json({ msg: "Price has be a number" });
+
+      const loggedInUser = req.currentUser;
+      const newProduct = await ProductModel.create({
+        ...req.body,
+        createdBy: loggedInUser._id,
+      });
+
+      await UserModel.findOneAndUpdate(
+        { _id: loggedInUser._id },
+        { $push: { products: newProduct._id } }
+      );
+
+      return res.status(201).json(newProduct);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  }
+);
+
+export { productRouter };
