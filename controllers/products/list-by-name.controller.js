dffa2a57805ff0ee47580932cabff0d957cf7f11
Joey Quadros <joey_qpb@hotmail.com>
2023-03-02 16:16:53 -0300

    feat: add list by name controller

---
+++ controllers/products/list-by-name.controller.js
@@ -0,0 +1,25 @@
+import { ProductModel } from "../../model/product.model.js";
+
+export const listProductByNameController = {
+  async handle(req, res) {
+    try {
+      const products = await ProductModel.find({});
+      if (!products)
+        return res.status(404).json({ msg: "no product has been registered" });
+
+      const listOrderByName = products.sort((a, b) => {
+        if (a.name < b.name) {
+          return -1;
+        }
+        if (a.name > b.name) {
+          return 1;
+        }
+        return 0;
+      });
+      return res.status(200).json(listOrderByName);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+};
