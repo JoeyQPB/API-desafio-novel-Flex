2c8b9dfb9ab32748a3f16b3cdf3b5762e48cde7f
Joey Quadros <joey_qpb@hotmail.com>
2023-03-02 05:17:07 -0300

    feat: add update partial product controller

---
+++ controllers/products/update-partial.controller.js
@@ -0,0 +1,21 @@
+import { ProductModel } from "../../model/product.model.js";
+
+export const updatePartialProductController = {
+  async updatePartial(req, res) {
+    try {
+      const loggedInUser = req.currentUser;
+      const updatedPartialProduct = await ProductModel.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          ...req.body,
+          $push: { updatedAt: Date.now(), updatedBy: loggedInUser._id },
+        },
+        { new: true, runValidators: true }
+      );
+      return res.status(200).json(updatedPartialProduct);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+};
