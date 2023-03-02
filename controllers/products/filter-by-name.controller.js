27588b00e32a346c4fea4f65ced8e4153c04f3ef
Joey Quadros <joey_qpb@hotmail.com>
2023-03-02 20:22:40 -0300

    feat: add filter by name controller

---
+++ controllers/products/filter-by-name.controller.js
@@ -0,0 +1,22 @@
+import { ProductModel } from "../../model/product.model.js";
+
+export const filterByNameController = {
+  async handle(req, res) {
+    try {
+      if (!req.body.inputName || typeof req.body.inputName !== "string") {
+        return res.status(400).json({
+          msg: `"${req.body.inputName}" is not a valid input. inputName has to be defined and be a string!`,
+        });
+      }
+
+      const product = await ProductModel.find({ name: req.body.inputName });
+      if (product.length < 1)
+        return res.status(404).json({ msg: "Product not found" });
+
+      return res.status(200).json(product);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+};
