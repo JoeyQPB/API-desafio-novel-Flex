4806439b6985d2d60de23b92ef99d02108a41484
Joey Quadros <joey_qpb@hotmail.com>
2023-03-04 19:08:03 -0300

    feat: add mocks to test

---
+++ controllers/products/list-Product/by-name/test/mocks/product.mock.js
@@ -0,0 +1,21 @@
+export const products = [
+  {
+    _id: "1",
+    name: "C",
+    description: "Product C description",
+    price: 5,
+  },
+  {
+    _id: "2",
+    name: "B",
+    description: "Product B description",
+    price: 10,
+  },
+
+  {
+    _id: "2",
+    name: "A",
+    description: "Product A description",
+    price: 15,
+  },
+];
---
+++ controllers/products/list-Product/by-price/test/mocks/product.mock.js
@@ -0,0 +1,20 @@
+export const products = [
+  {
+    _id: "1",
+    name: "Product 1",
+    description: "Product 1 description",
+    price: 15,
+  },
+  {
+    _id: "2",
+    name: "Product 2",
+    description: "Product 2 description",
+    price: 10,
+  },
+  {
+    _id: "3",
+    name: "Product 3",
+    description: "Product 3 description",
+    price: 5,
+  },
+];
